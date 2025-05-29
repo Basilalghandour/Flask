8275927c533ce40d7c7200300bc29c16f12543c7
GhandouR <basilelghandour@gmail.com>
2025-05-29 18:22:02 +0300

    Done Untill Item Selling !

--- market/forms.py
+++ market/forms.py
@@ -27,4 +27,11 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
     password = PasswordField(label='Password:', validators=[DataRequired()])
-    submit = SubmitField(label='Sign in')    
+    submit = SubmitField(label='Sign in')   
+    
+    
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label='Purchase Item')     
+    
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label='Sell Item')         
